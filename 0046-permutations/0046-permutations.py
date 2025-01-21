6e36216928ce70b9c3a8af3ce112305e5003e4df
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-01-21 17:32:55 -0500

    Time: 0 ms (100%), Space: 18.2 MB (8.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,42 @@
 class Solution:
 
-    #Itterative:
+    #recursive Backtracking - DFS
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        perms = [[]]
+        res, sol = [], []
+        n = len(nums)
 
-        for n in nums:
-            new_perm = []
-
-            for p in perms:
-                for i in range(len(p)+1):
-                    p_copy = p.copy()
-                    p_copy.insert(i,n)
-                    new_perm.append(p_copy)
+        def backtrack():
+            if len(sol) == n:
+                res.append(sol.copy())
+                return
             
-            perms = new_perm
-        return perms
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+            
+        backtrack()
+        return res
+
+
+    #Itterative:
+    # def permute(self, nums: List[int]) -> List[List[int]]:
+
+    #     perms = [[]]
+
+    #     for n in nums:
+    #         new_perm = []
+
+    #         for p in perms:
+    #             for i in range(len(p)+1):
+    #                 p_copy = p.copy()
+    #                 p_copy.insert(i,n)
+    #                 new_perm.append(p_copy)
+            
+    #         perms = new_perm
+    #     return perms
     
 
 
