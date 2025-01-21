cd1743c56d2239bd66d48781be34b2ef3756b7a6
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-01-21 08:29:53 -0500

    Time: 0 ms (100%), Space: 18.1 MB (12.46%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -10,14 +10,16 @@ class Solution:
                 res.append(subset.copy())
                 return
             
-            #left
+            #decision to include nums[i]:
+
             subset.append(nums[i])
             dfs(i+1)
 
-            #right
+            #decision to add empty:
             subset.pop()
             dfs(i+1)
-
+        
         dfs(0)
         return res
+
         
