63d33dd5a8b1126c2d167eeab41ec392f745139f
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-02-28 21:06:26 -0500

    Time: 0 ms (100%), Space: 18 MB (46.47%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -6,18 +6,29 @@ class Solution:
                 nums[i] = nums[i]*2
                 nums[i+1] = 0
         
-        output = [0] *len(nums)
+        # output = [0] *len(nums)
 
-        pointer1 = 0
-        pointer2 = 0
+        # pointer1 = 0
+        # pointer2 = 0
 
-        while pointer1 < len(nums):
-            if nums[pointer1] != 0:
-                output[pointer2] = nums[pointer1]
-                pointer2+=1
+        # while pointer1 < len(nums):
+        #     if nums[pointer1] != 0:
+        #         output[pointer2] = nums[pointer1]
+        #         pointer2+=1
 
-            pointer1 += 1
+        #     pointer1 += 1
         
-        return output
+        # return output
+
+        left = 0
+
+        for right in range(len(nums)):
+            while left<right and nums[left]!=0:
+                left+=1
+
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+        
+        return nums
 
         
