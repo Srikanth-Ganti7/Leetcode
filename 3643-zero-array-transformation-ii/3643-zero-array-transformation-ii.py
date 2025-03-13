0283c0b7c9ff359c30cb4c69f4407834fd245695
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-03-13 15:48:47 -0400

    Time: 159 ms (81.72%), Space: 62.9 MB (94.58%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        line = [0] * (len(nums) + 1)
+        decrement = 0
+        k = 0
+
+        for i, num in enumerate(nums):
+            while decrement + line[i] < num:
+                if k == len(queries):
+                    return -1
+                l, r, val = queries[k]
+                k += 1
+                if r < i:
+                    continue
+                line[max(l, i)] += val
+                line[r + 1] -= val
+            decrement += line[i]
+
+        return k
+        
