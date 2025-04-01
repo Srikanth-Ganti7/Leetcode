02d2a32cc3c1f3e9d3cbfb7c171c32463504affd
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-04-01 16:14:11 -0400

    Time: 141 ms (42.76%), Space: 67.6 MB (31.97%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        cache = [0] * len(questions)
+
+        def backtracking(i):
+            if i >= len(questions):
+                return 0
+            
+            if cache[i]:
+                return cache[i]
+            
+            points, brainpower = questions[i]
+
+            cache[i] = max(
+                backtracking(i+1), points + backtracking(i+1+brainpower)
+            )
+            
+            return cache[i]
+        
+        return backtracking(0)
+
+
+        
