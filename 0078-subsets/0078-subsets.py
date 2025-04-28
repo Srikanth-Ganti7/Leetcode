22c1707255c757013fab55f60ea1071927b3a787
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-04-28 17:04:11 -0400

    Time: 0 ms (100%), Space: 17.8 MB (90.65%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,25 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-
         res = []
-
-        subset = []
+        cur = []
 
         def dfs(i):
             if i >= len(nums):
-                res.append(subset.copy())
+                res.append(cur.copy())
                 return
             
-            #decision to include nums[i]:
-
-            subset.append(nums[i])
+            #include nums[i]
+            cur.append(nums[i])
             dfs(i+1)
 
-            #decision to add empty:
-            subset.pop()
+            #not include
+            cur.pop()
             dfs(i+1)
-        
         dfs(0)
         return res
-
         
