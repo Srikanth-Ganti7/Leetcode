c040ff943314d39a7664127f94cf7478cd2ca212
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-05-15 14:18:04 -0400

    Time: 42 ms (35.18%), Space: 19.5 MB (30.05%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,31 +4,34 @@ class Solution:
         ROWS = len(heights)
         COLS = len(heights[0])
 
-        pacificVisit = set()
-        atlanticVisit = set()
+        pac = set()
+        atl = set()
+
 
         def dfs(r,c,visit, prevHeight):
-            if (r<0 or c<0 or r== ROWS or c == COLS or heights[r][c]<prevHeight or (r,c) in visit):
+            if (r<0 or r>= ROWS or c<0 or c>=COLS or (r,c) in visit or heights[r][c] < prevHeight):
                 return
+            
             visit.add((r,c))
-            dfs(r+1,c, visit, heights[r][c])
-            dfs(r-1,c, visit, heights[r][c])
-            dfs(r,c+1, visit, heights[r][c])
-            dfs(r,c-1, visit, heights[r][c])
 
+            dfs(r+1,c,visit,heights[r][c])
+            dfs(r,c+1,visit,heights[r][c])
+            dfs(r-1,c,visit,heights[r][c])
+            dfs(r,c-1,visit,heights[r][c])
+        
         for r in range(ROWS):
-            dfs(r, 0, pacificVisit, heights[r][0])
-            dfs(r, COLS-1, atlanticVisit, heights[r][COLS-1])
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS-1, atl, heights[r][COLS-1])
         
         for c in range(COLS):
-            dfs(0, c, pacificVisit, heights[0][c])
-            dfs(ROWS-1, c, atlanticVisit, heights[ROWS-1][c])
-
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
         res = []
-
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) in atlanticVisit and (r,c) in pacificVisit:
+                if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
         
         return res
+
+        
