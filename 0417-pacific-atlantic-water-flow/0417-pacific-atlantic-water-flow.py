c4e712a1faece425c3b5693328a375141997fef1
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-05-16 08:48:46 -0400

    Time: 35 ms (57.1%), Space: 19.1 MB (91.35%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -7,18 +7,17 @@ class Solution:
         pac = set()
         atl = set()
 
-
         def dfs(r,c,visit, prevHeight):
-            if (r<0 or r>= ROWS or c<0 or c>=COLS or (r,c) in visit or heights[r][c] < prevHeight):
+            if (r<0 or r>=ROWS or c<0 or c>=COLS or (r,c) in visit or heights[r][c]< prevHeight):
                 return
             
             visit.add((r,c))
+            dfs(r+1,c,visit, heights[r][c])
+            dfs(r,c+1,visit, heights[r][c])
+            dfs(r-1,c,visit, heights[r][c])
+            dfs(r,c-1,visit, heights[r][c])
 
-            dfs(r+1,c,visit,heights[r][c])
-            dfs(r,c+1,visit,heights[r][c])
-            dfs(r-1,c,visit,heights[r][c])
-            dfs(r,c-1,visit,heights[r][c])
-        
+
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS-1, atl, heights[r][COLS-1])
@@ -33,5 +32,6 @@ class Solution:
                     res.append([r,c])
         
         return res
+
 
         
