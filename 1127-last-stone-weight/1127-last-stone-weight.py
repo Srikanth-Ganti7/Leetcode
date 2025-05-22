81641c5da3a39e52254ba97164a3ee5211334832
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-05-21 23:03:01 -0400

    Time: 0 ms (100%), Space: 17.9 MB (10.19%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -4,13 +4,14 @@ class Solution:
 
         heapq.heapify(stones)
 
-        while len(stones) > 1:
+        while len(stones)>1:
             first = heapq.heappop(stones)
             second = heapq.heappop(stones)
 
-            if second > first:
-                val = -1 * (abs(first) - abs(second))
-                heapq.heappush(stones, val)
+            if second>first:
+                heapq.heappush(stones, first-second)
+        
         stones.append(0)
         return abs(stones[0])
+
         
