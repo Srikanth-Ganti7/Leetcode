7dbc813e11f5e0cc9905e481f3521927365a1776
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-06-13 17:27:46 -0400

    Time: 0 ms (100%), Space: 18.8 MB (51.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+
         Hmap = {}
 
         for i in range(len(nums)):
             if target - nums[i] in Hmap:
-                return [i, Hmap[target - nums[i]]]
+                return [i, Hmap[target-nums[i]]]
             
             else:
                 Hmap[nums[i]] = i
-        
-
         
