9ef83014a11a22737958be1023bbf86c5fe2e7f7
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-06-25 14:04:32 -0400

    Time: 3 ms (97.74%), Space: 19.4 MB (19.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,27 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        left = 0
+        right = len(height) - 1
 
-        leftPointer = 0
-        rightPointer = len(height) - 1
+        left_max = 0
+        right_max = 0
 
-        res = 0
+        water = 0
 
-        leftMax = height[leftPointer]
-        rightMax = height[rightPointer]
-
-        while leftPointer < rightPointer:
-            if leftMax < rightMax:
-                leftPointer += 1
-                leftMax = max(leftMax, height[leftPointer])
-                res += leftMax - height[leftPointer]
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
             
             else:
-                rightPointer -= 1
-                rightMax = max(rightMax, height[rightPointer])
-                res += rightMax - height[rightPointer]
-        return res
-
-             
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    water += right_max - height[right]
+                
+                right -= 1
+        return water
         
