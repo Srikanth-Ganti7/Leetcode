999c5de57c9ea8605140baa90b6328354a13705c
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-07-10 08:47:08 -0400

    Time: 8 ms (9.95%), Space: 18.3 MB (18.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -4,36 +4,33 @@ class Solution:
         B = nums2
 
         if len(nums1) > len(nums2):
-            A,B = B,A
+            A, B = B, A
 
+        
         total = len(nums1) + len(nums2)
         half = total // 2
 
         l = 0
-        r = len(A) - 1
-
+        r = len(A) - 1 
         while True:
             i = (l+r) // 2
             j = half - i - 2
 
-            Aleft = A[i] if i >=0 else float("-inf")
+            Aleft = A[i] if i >= 0 else float("-inf")
             Bleft = B[j] if j >= 0 else float("-inf")
 
-            Aright =  A[i+1] if i+1 < len(A) else float("inf")
-            Bright = B[j+1] if j+1 < len(B) else float("inf")
+            Aright = A[i+1] if i+1 < len(A) else float("inf")
+            Bright = B[j+1] if j +1 < len(B) else float("inf")
 
             if Aleft <= Bright and Bleft <= Aright:
                 if total % 2:
-                    return min(Aright,Bright)
+                    return min(Aright, Bright)
                 else:
-                    return (max(Aleft, Bleft) + min(Aright, Bright))/ 2
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
             
-            elif Aleft > Bright:
-                r = i -1
+            elif Aright > Bleft:
+                r = i - 1
             
             else:
                 l = i + 1
-        
-
-
         
