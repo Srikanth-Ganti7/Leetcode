842fc42c9851ad711bd8b88086de9f649562a7fd
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-07-10 08:48:23 -0400

    Time: 3 ms (51.9%), Space: 18.2 MB (38.86%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -17,10 +17,10 @@ class Solution:
             j = half - i - 2
 
             Aleft = A[i] if i >= 0 else float("-inf")
-            Bleft = B[j] if j >= 0 else float("-inf")
-
             Aright = A[i+1] if i+1 < len(A) else float("inf")
-            Bright = B[j+1] if j +1 < len(B) else float("inf")
+
+            Bleft = B[j] if j >= 0 else float("-inf")
+            Bright = B[j+1] if j+1 < len(B) else float("inf")
 
             if Aleft <= Bright and Bleft <= Aright:
                 if total % 2:
