f960b74aaddb7a7e90cd27eda78bb28c0239a76b
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2025-12-24 13:24:57 -0500

    Time: 3 ms (12.62%), Space: 17.3 MB (96.18%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
@@ -1,15 +1,27 @@
 class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
 
-        totalSum = sum(apple)
-        capacity.sort(reverse = True)
-        count = 0
+        # totalSum = sum(apple)
+        # capacity.sort(reverse = True)
+        # count = 0
 
-        for cap in capacity:
-            totalSum -= cap
-            count += 1
-            if totalSum <=0:
-                return count
+        # for cap in capacity:
+        #     totalSum -= cap
+        #     count += 1
+        #     if totalSum <=0:
+        #         return count
+
+        totalSum = sum(apple)
+        maxHeap = [-c for c in capacity]
+        heapq.heapify(maxHeap)
+
+        boxes = 0
+
+        while totalSum > 0 and maxHeap:
+            totalSum += heapq.heappop(maxHeap)
+            boxes += 1
+        
+        return boxes
 
         
 
