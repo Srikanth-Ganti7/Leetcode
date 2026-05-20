3d4e64f3bbb35ec97881285acf4d489f1cb6e2a4
Srikanth-Ganti7 <96021374+Srikanth-Ganti7@users.noreply.github.com>
2026-05-20 17:42:13 -0400

    Time: 0 ms (100%), Space: 19.4 MB (10.15%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -13,8 +13,8 @@ class Solution:
         if not root1 or not root2 or root1.val != root2.val:
             return False
         
-        Flip = (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right))
-        noFlip = (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left))
+        noFlip = (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right))
+        Flip = (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left))
 
         return Flip or noFlip
         
